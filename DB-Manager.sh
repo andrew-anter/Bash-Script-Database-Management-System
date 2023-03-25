e873f8efaf822ccae7a0519bbb8def5d6f2cfa8d
Andrew Anter <andrew.anter@gmail.com>
2023-03-25 20:42:09 +0200

    Initial list made

--- DB-Manager.sh
+++ DB-Manager.sh
@@ -1,6 +1,18 @@
-#!/bin/etc
+#!/bin/bash
 
-while read -p "Select an option" option
+while read -p "Select an option
+	1- Create Database
+	2- List Databases
+	3- Connect to Database
+	4- Drop Database
+	5- Exit Application
+	" option
 do
-
+	case $option in 
+		"1")	echo "DB created";;
+		"2")	echo "DB listed";;
+		"3")	echo "DB Connected";;
+		"4")	echo "DB Dropped";;
+		"5")	exit;;
+	esac
 done
