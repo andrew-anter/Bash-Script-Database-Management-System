bffcba4895efb428e93dc38aab5dc2275cd998c7
Andrew Anter <andrew.anter@gmail.com>
2023-03-26 00:13:27 +0200

    List Tables and Drop Tables Added + Bug fixes

--- Table-Manager.sh
+++ Table-Manager.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+# check and create tables and metadata folders
+if ! [ -d "Tables" ]
+then
+	mkdir "Tables"
+	echo "Tables Directory Created"
+fi
+
+if ! [ -d "Metadata" ]
+then
+	mkdir "Metadata"
+	echo "Metadata Directory Created"
+fi
+
 echo "Greetings from Table Manager: $PWD"
+
+###########################################
 
 while read -p "
 Select an option:
@@ -17,19 +32,32 @@ do
 	case $option in 
 		"1") 
 			read -p "Enter table name: " tbName
-			if [ -f $tbName  ]
+			if [ -f "./Tables/$tbName"  ]
 			then
 				echo "Error this table already exist"
 			else
-				touch $tbName
+				touch "./Tables/$tbName"
 				echo "Table $tbName created"
 			fi
 			;;
 		"2")
-			echo "List tables"
+			ls "./Tables/"
 			;;
 		"3")
-			echo "Drop tables"
+			read -p "Enter table name: " tbName
+			
+			if [ -f "./Tables/$tbName"  ]
+			then
+				read -p "Do you really want to drop this table with its data? [y/n]: " confirm
+				if [ $confirm = "y"  ]
+				then
+					rm "./Tables/$tbName"
+					echo "Table Dropped"
+				fi
+
+			else
+				echo "This table does not exist"
+			fi
 			;;
 		"4") 
 			echo "Insert into table"
