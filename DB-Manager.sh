c5f3b6f47627f735814649f6796533065df476f8
Andrew Anter <andrew.anter@gmail.com>
2023-03-27 01:10:13 +0200

    Optimizations added

--- DB-Manager.sh
+++ DB-Manager.sh
@@ -7,7 +7,7 @@ then
 	echo "DBS created"
 fi
 
-while read -p "
+while read -r -p "
 Select an option:
 	1- Create Database
 	2- List Databases
@@ -18,10 +18,10 @@ Select an option:
 do
 	case $option in 
 		"1")	
-			read -p "Enter the name of the Database:[must start with an alhabet] " dbName
+			read -p -r "Enter the name of the Database:[must start with an alhabet] " dbName
 			if [[ $dbName =~ ^[A-Za-z]+[0-9]* ]]
 			then
-				if [ -d "./DBS/$dbName" ] && [ $dbName  ]
+				if [ -d "./DBS/$dbName" ] && [ "$dbName"  ]
 				then
 					echo "!Error: Database is already exist!"
 				else
@@ -38,12 +38,12 @@ do
 			;;
 
 		"3")	
-			read -p "Enter the name of the Database to connect to: " dbName
-			if [ -d "./DBS/$dbName"  ] && [ $dbName  ]
+			read -p -r "Enter the name of the Database to connect to: " dbName
+			if [ -d "./DBS/$dbName"  ] && [ "$dbName"  ]
 			then
-				cd "./DBS/$dbName"
+				cd "./DBS/$dbName" || exit
 				echo "Databse $dbName Connected"
-				../../Table-Manager.sh $dbName
+				../../Table-Manager.sh "$dbName"
 				cd ../..
 			else
 				echo "!Error: Database does not exist!"
@@ -51,11 +51,11 @@ do
 			;;
 
 		"4")	
-			read -p "Enter the name of the Database to drop: " dbName
-			if [ -d "./DBS/$dbName"  ] && [ $dbName  ]
+			read -p -r "Enter the name of the Database to drop: " dbName
+			if [ -d "./DBS/$dbName"  ] && [ "$dbName"  ]
 			then
-				read -p "Are you sure want to drop $dbName Database with all its tables? [y/n]: " check
-				if [ ${check^^} = "Y" ]
+				read -p -r "Are you sure want to drop $dbName Database with all its tables? [y/n]: " check
+				if [ "${check^^}" = "Y" ]
 				then
 					rm -r "./DBS/$dbName"
 					echo "Database $dbName dropped."
